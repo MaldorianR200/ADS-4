baa5bd9f3c2ee9397d84ed283b02f5d274ce7458
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-03-19 21:53:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ int sizeEdge (int* arr, int size, int value) {
   }
   return 0;
 }
-int cbinsearch(int* arr, int size, int value) {
+int cbinsearch (int* arr, int size, int value) {
   int count = 0;
   int first = 0;
   int last = size - 1;
@@ -36,7 +36,7 @@ int cbinsearch(int* arr, int size, int value) {
   }
   return 0;
 }
-int countPairs1(int *arr, int len, int value) {
+int countPairs1 (int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
@@ -47,7 +47,7 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2 (int *arr, int len, int value) {
   int count = 0;
   len = sizeEdge(arr, len, value);
   for (int i = 0; i < len; ++i) {
@@ -57,7 +57,7 @@ int countPairs2(int *arr, int len, int value) {
   }
   return count;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3 (int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     int n = value - arr[i];
